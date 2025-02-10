c63f3e8ccfc569b2c49033dca7684691d18ce80e
AlvaroDiazTorres <adtclase@gmail.com>
2025-02-10 13:01:19 +0100

    first commit

--- src/pages/OfferList.tsx
+++ src/pages/OfferList.tsx
@@ -1,25 +1,46 @@
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import Offer from '../models/Offer'
 import { OfferService } from '../services/offer.services'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
+import toast from 'react-hot-toast'
 
 function OfferList() {
   const [offers, setOffers] = useState<Offer[]>()
-  const [error, setError] = useState(null)
+  const [error, setError] = useState<string | null> (null)
   const [loading, setLoading] = useState(true)
+  const[queryParams, setQueryParams]=useSearchParams()
+  const titleQuery = queryParams.get("title") || ''
+
   useEffect(()=>{
     OfferService.getAll()
           .then(setOffers)
           .catch((error)=>setError(error.message))
           .finally(()=>setLoading(false))
     
-  },[])
+  },[titleQuery])
 
-  
+  const handleSearchChange = (e:ChangeEvent<HTMLInputElement>) =>{
+    const newTitle = e.target.value
+    setQueryParams(newTitle ? {title: newTitle} : {})
+  }
+
+  const handleDelete = async (id:number) =>{
+    if(!window.confirm('¿Estás seguro de que quieres borrar esta oferta?')) return
+    toast.error('Error bobo')
+    try{
+      await OfferService.delete(id)
+      setOffers(offers?.filter(offer => offer.id !== id))
+      toast.success('Oferta borrada correctamente!')
+    }catch(error){
+      setError(error instanceof Error ? error.message : 'Error desconocido')
+
+    }
+  }
   return (
     <div  className='text-white'>
       <h1>Lista de ofertas</h1>
       <Link to="/offers/new">Añadir nueva oferta</Link>
+      <input value={titleQuery} onChange={handleSearchChange} placeholder='Buscar por título'/>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {offers?.length === 0 && <p>No hay ofertas disponibles</p>}
@@ -28,7 +49,7 @@ function OfferList() {
             {offer.title}
             <Link to={`/offers/${offer.id}`}>Ver</Link>
             <Link to={`/offers/edit/${offer.id}`}>Editar</Link>
-            <button>Borrar</button>
+            <button onClick={() => handleDelete(offer.id)}>Borrar</button>
           </div>
       )}
       
--- src/services/offer.services.ts
+++ src/services/offer.services.ts
@@ -3,8 +3,10 @@ import { fetchAPI } from "../utils/FetchAPI"
 const API_URL_BASE = import.meta.env.VITE_API_URL_BASE
 
 export class OfferService {
-    static async getAll() {
-        return await fetchAPI(API_URL_BASE+'/offers')
+    static async getAll(title?:string) {
+        let url = API_URL_BASE+'/offers'
+        if(title) url+= '?title='+title
+        return await fetchAPI(url)
     }
 
     static async getById(id:number) {
@@ -30,6 +32,16 @@ export class OfferService {
             },
             body: JSON.stringify(offer),
             credentials: 'include'
+        })
+    }
+
+    static async delete(id:number){
+        return await fetchAPI(API_URL_BASE+"/offers/"+id,{
+            method: 'DELETE',
+            headers: {
+                "Content-Type":"application/json"
+            },
+            credentials:'include'
         })
     }
 
